e141b81580a90d04581fa1f91badf6be38352717
knishizaki <51872646+knishizaki@users.noreply.github.com>
2020-05-23 23:00:53 +0900

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,9 +1,6 @@
 import sys
 
 def main(lines):
-    # このコードは標準入力と標準出力を用いたサンプルコードです。
-    # このコードは好きなように編集・削除してもらって構いません。
-    # ---
     # This is a sample code to use stdin and stdout.
     # Edit and remove this code as you like.
 
